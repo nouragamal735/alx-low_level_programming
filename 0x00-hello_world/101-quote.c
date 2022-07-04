d522709a63cd296df45135f527bbca99b76b7b90
nouragamal735 <nouragamal735@gmail.com>
2022-07-04 04:47:02 -0700

    task-8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
+#include<unistd.h>
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",62);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62);
 	return (1);
 }
