ae409a0fe9218cc183783ac6c731b6fc16b8aa14
nouragamal735 <nouragamal735@gmail.com>
2022-07-07 11:22:52 -0700

    task-4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - program that prints the alphabet in lowercase except q,e
+ *Return: (0)
+ */
+int main(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+if (x != 'q' || x != 'e')
+{
+putchar(x);
+}
+}
+putchar('\n');
+return (0);
+}
