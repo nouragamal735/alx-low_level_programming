7ba4c348e67fbbfc9079bb4ad5b742910265880d
nouragamal735 <nouragamal735@gmail.com>
2022-07-07 12:07:14 -0700

    task-5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - program that prints all single digits of starting from 0
+ *Return: (0)
+ */
+int main(void)
+{
+char x;
+for (x = 0; x <= 10; x++)
+{
+printf("%d", n);
+}
+putchar('\n');
+return (0);
+}
