940e737568cc7479563d39a1a3f26b8acecec16f
nouragamal735 <nouragamal735@gmail.com>
2022-07-07 09:05:29 -0700

    task-3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 char x;
-for (x = 'a'; a <= 'z'; a++)
+for (x = 'a'; x <= 'z'; x++)
 putchar(x);
-for (x = 'A'; a <= 'Z'; a++)
+for (x = 'A'; x <= 'Z'; x++)
 putchar(x);
 putchar('\n');
 return (0);
