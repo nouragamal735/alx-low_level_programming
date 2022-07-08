b2b6268f0df7917467cd253cf57020d367e41c49
nouragamal735 <nouragamal735@gmail.com>
2022-07-07 20:46:40 -0700

    task-10

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+/**
+ *main - program that prints all possible combinations of three digits
+ *Return: (0)
+*/
+int main(void)
+{
+int x;
+int y;
+int z;
+for (x = 0; x < 10; x++)
+{
+for (y = 0; y < 10; y++)
+{
+for (z = 0; z < 10; z++)
+{
+if (z > y > x)
+{
+putchar('0' + x);
+putchar('0' + y);
+putchar('0' + z);
+if (z != 9 || y != 8 || x != 7)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
