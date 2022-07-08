a06ab2bebe7170da87f6d7581bd22145b6ddf5a0
nouragamal735 <nouragamal735@gmail.com>
2022-07-07 21:19:58 -0700

    task-11

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,13 +15,17 @@ for (y = 0; y < 10; y++)
 {
 for (z = 0; z < 10; z++)
 {
+for (f = 0; f < 10; f++)
+{
 putchar('0' + x);
 putchar('0' + y);
 putchar('0' + z);
-if (x != 9 || y != 9 || z != 9)
+putchar('0' + f);
+if (x != 9 || y != 9 || z != 9 || f != 9)
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
