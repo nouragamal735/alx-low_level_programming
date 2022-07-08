7712ac9d6b6ad2ac7fa6376d9aedba321bbcb1f3
nouragamal735 <nouragamal735@gmail.com>
2022-07-08 08:03:15 -0700

    task-0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,15 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-void positive_or_negative(int i){
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	printf("%d is positive", n)
-	return (0);
-}
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
